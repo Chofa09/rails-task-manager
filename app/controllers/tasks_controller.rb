3b6212b723d53342ae75537014ffd800d31660f6
Sophie Watson <semwatson09@gmail.com>
2023-08-22 13:10:34 +0200

    SLowly building framework

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,34 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    if @task.completed == true
+      @message = 'This task is completed'
+    else
+      @message = 'This task is not completed yet'
+    end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
