375685b65b5b5f6bd1aa2db87980baf6b640365e
Sophie Watson <semwatson09@gmail.com>
2023-08-22 14:48:20 +0200

    Functionality completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,7 +23,20 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
 
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
   end
 
   private
